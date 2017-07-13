2626072bc0716d6be51d89c4c665387f4f44669f
金晓东 <eric4336355@126.com>
2017-07-13 15:13:32 +0800

    生成环境搭建

--- src/component/about.vue
+++ src/component/about.vue
@@ -2,15 +2,12 @@
     <div id="about">
         <h3>关于我</h3>
         <ul class="about-wrap">
-            <li>前端码农</li>
+            <li>浪漫极客</li>
             <li>博客：
-                <a style="color: #32D3C3;" href="http://lweiwei.com">https://lweiwei.com</a>
+                <a style="color: #32D3C3;" href="http://blog.jxderic.xyz/">http://blog.jxderic.xyz/</a>
             </li>
             <li>github：
-                <a style="color: #32D3C3;" href="https://github.com/linguowei">https://github.com/linguowei</a>
-            </li>
-            <li>segmentfault：
-                <a style="color: #32D3C3;" href="https://segmentfault.com/u/linguowei">https://segmentfault.com/u/linguowei</a>
+                <a style="color: #32D3C3;" href="https://github.com/jxderic">https://github.com/jxderic</a>
             </li>
         </ul>
     </div>
